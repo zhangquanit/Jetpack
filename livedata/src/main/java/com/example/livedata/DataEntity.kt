46f0084d21fbcad53b0dfb6bd69f4a208e6e834d
zhangquan <zhangquanit@126.com>
2020-11-04 11:49:06 +0800

    Initial commit

---
+++ livedata/src/main/java/com/example/livedata/DataEntity.kt
@@ -0,0 +1,12 @@
+package com.example.livedata
+
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ *
+ * @author zhangquan
+ */
+data class DataEntity(val str:String?){
+
+}
---
+++ livedata/src/main/java/com/example/livedata/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var liveData= MutableLiveData<DataEntity>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        liveData.observe(this, Observer {
+            println("11 收到通知: ${it.str}")
+        })
+
+        btn_send.setOnClickListener {
+            //发送通知
+            liveData.value=DataEntity("hellow")
+        }
+        btn_regist.setOnClickListener {
+            //注册observer，同一个LifecycleOwner
+            liveData.observe(MainActivity@this, Observer {
+                println("22 收到通知: ${it.str}")
+            })
+        }
+    }
+}
